63c09545962e2ddc36f15c587749fea186d87df5
GustavoPadilha08 <108557193+GustavoPadilha08@users.noreply.github.com>
2022-09-27 22:22:57 -0300

    Update livraria.sql

--- livraria.sql
+++ livraria.sql
@@ -1,10 +1,26 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 28-Set-2022 às 03:21
+-- Versão do servidor: 10.4.24-MariaDB
+-- versão do PHP: 7.4.29
 
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Banco de dados: `livraria`
 --
 
-CREATE database livraria;
 -- --------------------------------------------------------
 
 --
@@ -13,8 +29,8 @@ CREATE database livraria;
 
 CREATE TABLE `editora` (
   `codigo_editora` int(11) NOT NULL,
-  `nome_editora` text 
-);
+  `nome_editora` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -23,9 +39,20 @@ CREATE TABLE `editora` (
 --
 
 CREATE TABLE `generos` (
-  `nome_genero` text,
-  `codigo_genero` int(11) NOT NULL
-);
+  `codigo_genero` int(11) NOT NULL,
+  `nome_genero` varchar(120) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Extraindo dados da tabela `generos`
+--
+
+INSERT INTO `generos` (`codigo_genero`, `nome_genero`) VALUES
+(1, 'Biografias'),
+(2, 'Literatura'),
+(3, 'AutoAjuda'),
+(4, 'Didático'),
+(5, 'Quadrinhos');
 
 -- --------------------------------------------------------
 
@@ -35,14 +62,14 @@ CREATE TABLE `generos` (
 
 CREATE TABLE `livros` (
   `codigo_livro` int(11) NOT NULL,
-  `titulo_livro` varchar(120),
-  `autor_livro` varchar(120),
-  `cod_editora` int(11),
-  `cod_genero` int(11),
-  `cod_subgenero` int(11),
-  `resumo` text,
-  `texto` float 
-);
+  `titulo_livro` varchar(120) DEFAULT NULL,
+  `autor_livro` varchar(120) DEFAULT NULL,
+  `cod_editora` int(11) DEFAULT NULL,
+  `cod_genero` int(11) DEFAULT NULL,
+  `cod_subgenero` int(11) DEFAULT NULL,
+  `resumo` text DEFAULT NULL,
+  `texto` float DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -52,6 +79,11 @@ CREATE TABLE `livros` (
 
 CREATE TABLE `subgenero` (
   `codigo_subgenero` int(11) NOT NULL,
-  `nome_subgenero` text,
-  `cod_genero_pertencente` int(11) 
-);
+  `nome_subgenero` text DEFAULT NULL,
+  `cod_genero_pertencente` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
