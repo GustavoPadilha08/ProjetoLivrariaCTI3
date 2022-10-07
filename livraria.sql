2ecc0b4f32de6285693428a42e18fb7237025e65
GustavoPadilha08 <108557193+GustavoPadilha08@users.noreply.github.com>
2022-10-07 20:08:06 -0300

    Update livraria.sql

--- livraria.sql
+++ livraria.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 01-Out-2022 às 01:46
+-- Tempo de geração: 05-Out-2022 às 03:20
 -- Versão do servidor: 10.4.22-MariaDB
 -- versão do PHP: 8.1.2
 
@@ -18,8 +18,23 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Banco de dados: `projeto_livraria_joao`
+-- Banco de dados: `livraria2`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `acervo`
+--
+
+CREATE TABLE `acervo` (
+  `ID` int(11) NOT NULL,
+  `Titulo` varchar(120) DEFAULT NULL,
+  `Autor` varchar(120) DEFAULT NULL,
+  `Editora` varchar(120) DEFAULT NULL,
+  `Quantidade` int(11) DEFAULT NULL,
+  `Id_localizacao` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -28,18 +43,10 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `editora` (
-  `codigo_editora` int(11) NOT NULL,
-  `nome_editora` text DEFAULT NULL
+  `codigo_editora` int(11) DEFAULT NULL,
+  `nome_editora` varchar(120) DEFAULT NULL,
+  `cidade_editora` varchar(120) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Extraindo dados da tabela `editora`
---
-
-INSERT INTO `editora` (`codigo_editora`, `nome_editora`) VALUES
-(1, 'Viena'),
-(2, 'Livro Bom'),
-(3, 'Leia bem');
 
 -- --------------------------------------------------------
 
@@ -48,20 +55,34 @@ INSERT INTO `editora` (`codigo_editora`, `nome_editora`) VALUES
 --
 
 CREATE TABLE `generos` (
-  `codigo_genero` int(11) NOT NULL,
-  `nome_genero` varchar(120) NOT NULL
+  `cod_genero` int(11) DEFAULT NULL,
+  `nome_genero` varchar(120) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Extraindo dados da tabela `generos`
 --
 
-INSERT INTO `generos` (`codigo_genero`, `nome_genero`) VALUES
+INSERT INTO `generos` (`cod_genero`, `nome_genero`) VALUES
 (1, 'Biografias'),
 (2, 'Literatura'),
-(3, 'AutoAjuda'),
+(3, 'Autoajuda'),
 (4, 'Didático'),
 (5, 'Quadrinhos');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `leitores`
+--
+
+CREATE TABLE `leitores` (
+  `ID` int(11) NOT NULL,
+  `Nome` varchar(120) DEFAULT NULL,
+  `Telefone` varchar(120) DEFAULT NULL,
+  `Turma` int(11) DEFAULT NULL,
+  `Data_de_cadastro` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -70,27 +91,147 @@ INSERT INTO `generos` (`codigo_genero`, `nome_genero`) VALUES
 --
 
 CREATE TABLE `livros` (
-  `codigo_livro` int(11) NOT NULL,
-  `titulo_livro` varchar(120) DEFAULT NULL,
-  `autor_livro` varchar(120) DEFAULT NULL,
-  `cod_editora` int(11) DEFAULT NULL,
-  `cod_genero` int(11) DEFAULT NULL,
-  `cod_subgenero` int(11) DEFAULT NULL,
-  `resumo` text DEFAULT NULL,
-  `texto` float DEFAULT NULL
+  `codigo` int(11) NOT NULL,
+  `titulo` varchar(120) DEFAULT NULL,
+  `autor` varchar(120) DEFAULT NULL,
+  `Editora` varchar(120) DEFAULT NULL,
+  `genero` varchar(120) DEFAULT NULL,
+  `subgenero` varchar(120) DEFAULT NULL,
+  `resumo` varchar(120) DEFAULT NULL,
+  `preco` float DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `subgenero`
+-- Estrutura da tabela `localização`
 --
 
-CREATE TABLE `subgenero` (
-  `codigo_subgenero` int(11) NOT NULL,
-  `nome_subgenero` text DEFAULT NULL,
-  `cod_genero_pertencente` int(11) DEFAULT NULL
+CREATE TABLE `localização` (
+  `ID` int(11) NOT NULL,
+  `Nome` varchar(120) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `reserva`
+--
+
+CREATE TABLE `reserva` (
+  `id` int(11) NOT NULL,
+  `data_inicio` date DEFAULT NULL,
+  `data_devolucao` date DEFAULT NULL,
+  `id_leitor` int(11) DEFAULT NULL,
+  `id_acervo` int(11) DEFAULT NULL,
+  `observação` varchar(120) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `subgeneros`
+--
+
+CREATE TABLE `subgeneros` (
+  `cod` int(11) DEFAULT NULL,
+  `nome` varchar(120) DEFAULT NULL,
+  `codigogenero` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `usuario`
+--
+
+CREATE TABLE `usuario` (
+  `ID` int(11) NOT NULL,
+  `NOME` varchar(120) DEFAULT NULL,
+  `EMAIL` varchar(120) DEFAULT NULL,
+  `TELEFONE` varchar(120) DEFAULT NULL,
+  `SENHA` varchar(120) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices para tabela `acervo`
+--
+ALTER TABLE `acervo`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Índices para tabela `leitores`
+--
+ALTER TABLE `leitores`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Índices para tabela `livros`
+--
+ALTER TABLE `livros`
+  ADD PRIMARY KEY (`codigo`);
+
+--
+-- Índices para tabela `localização`
+--
+ALTER TABLE `localização`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Índices para tabela `reserva`
+--
+ALTER TABLE `reserva`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices para tabela `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- AUTO_INCREMENT de tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `acervo`
+--
+ALTER TABLE `acervo`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `leitores`
+--
+ALTER TABLE `leitores`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `livros`
+--
+ALTER TABLE `livros`
+  MODIFY `codigo` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `localização`
+--
+ALTER TABLE `localização`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `reserva`
+--
+ALTER TABLE `reserva`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `usuario`
+--
+ALTER TABLE `usuario`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
